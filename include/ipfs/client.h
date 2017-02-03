b5aeacd13636a1cc0e66bea1ab40533dd66cd9d3
iBad <hayk@hayotsyan.com>
2017-02-03 17:36:48 +0300

    build error

--- include/ipfs/client.h
+++ include/ipfs/client.h
@@ -21,7 +21,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 #define IPFS_CLIENT_H
 
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include <json.hpp>
 #include <string>
 #include <utility>
 #include <vector>
--- src/client.cc
+++ src/client.cc
@@ -18,7 +18,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include <json.hpp>
 #include <sstream>
 #include <stdexcept>
 #include <string>
