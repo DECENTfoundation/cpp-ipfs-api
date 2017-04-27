224ed7479967155564ec99814a86f0593c47d98e
Josef Sevcik <josef.sevcik@gmail.com>
2017-04-27 17:55:06 +0200

    reverting to http1.0

--- src/http/transport-curl.cc
+++ src/http/transport-curl.cc
@@ -106,7 +106,8 @@ void TransportCurl::Fetch(const std::string& url,
   curl_easy_setopt(curl_, CURLOPT_POST, 1);
   curl_easy_setopt(curl_, CURLOPT_LOW_SPEED_LIMIT, 1);
   curl_easy_setopt(curl_, CURLOPT_LOW_SPEED_TIME, 1800);
-
+  curl_easy_setopt(curl_, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+  
   curl_httppost* form_parts = NULL;
   curl_httppost* form_parts_end = NULL;
 
